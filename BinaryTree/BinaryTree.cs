aca79927147ed54c32ada7a4f4ab9aa4eacea369
Lisa Popodi <lisa@lisapopodi.com>
2017-04-24 10:24:06 -0500

    adding files to github

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BST
+    {
+        public Node root;
+
+        public BST()
+        {
+            root = null;
+        }
+
+        public void Add(int value)
+        {
+            Node newNode = new Node();
+            newNode.value = value;
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+
+                while (true)
+                {
+                    parent = current;
+                    if (value < current.value)
+                    {
+                        current = current.left;
+
+                        if (current == null)
+                            parent.left = newNode;
+                        break;
+                    }
+                    // }
+                    else
+                    {
+                        current = current.right;
+
+                        if (current == null)
+                            parent.right = newNode;
+                        break;
+                    }
+                }
+            }
+        }  //end Add
+
+        public void Print(Node node)
+        {
+            if (node == null)
+                return;
+
+        }
+
+
+     } // end BST
+} // end namespace
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BST tree = new BST();
+            tree.Add(50);
+            tree.Add(40);
+            tree.Add(60);
+            tree.Add(20);
+            tree.Add(10);
+            tree.Add(90);
+            tree.Add(70);
+
+            Console.ReadLine();
+        }
+
+
+
+    }      
+}
